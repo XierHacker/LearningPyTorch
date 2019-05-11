1c7387cf1b3f5cd555d8f1e5d8505254d8162d01
XierHacker <xierhacker@163.com>
2019-05-11 15:08:57 +0800

    update

--- 4.Autograd/example2.py
+++ 4.Autograd/example2.py
@@ -6,7 +6,39 @@
 import numpy as np
 import torch
 
+N, D_in, H, D_out = 64, 1000, 100, 10
+LEARNING_RATE=1e-6
 
 
+# 随机创建一些训练数据
+x=torch.randn(size=(N,D_in))
+#print("x:\n",x.shape)
+y=torch.randn(size=(N,D_out))
+#print("y:\n",y.shape)
 
+#weights
+w1=torch.randn(size=(D_in,H))
+w2=torch.randn(size=(H,D_out))
 
+for it in range(500):
+    # Forward pass
+    h=torch.mm(x,w1)
+    h_relu=torch.clamp(h,min=0.0)
+    y_pred=torch.mm(h_relu,w2)
+
+    # compute loss
+    loss = (y_pred - y).pow(2).sum()
+    print(it, loss.numpy())
+
+    # Backward pass
+    # compute the gradient
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h < 0] = 0
+    grad_w1 = x.t().mm(grad_h)
+
+    # update weights of w1 and w2
+    w1 -= LEARNING_RATE * grad_w1
+    w2 -= LEARNING_RATE * grad_w2
